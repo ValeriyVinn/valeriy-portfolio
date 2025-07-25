d196d1c5710df07378f9656423e67699d381863b
ValeriyVinn <valeriy.all@gmail.com>
2025-07-26 00:34:48 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <h1>Резюме</h1>
+      </main>
+      <footer className="">
+       
+      </footer>
+    </div>
+  );
+}
